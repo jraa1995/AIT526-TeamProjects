5df46dca116adaa708982d12562c110640a09a74
AllyRohrer <117534775+AllyRohrer@users.noreply.github.com>
2024-06-22 21:51:20 -0400

    Update qa-system.py
    The main updates I made where adding code so that where are when questions with common formats are answered.  I did this by adding more regex to the code.  I think that before sunday we just need to focus on adding more regex's to the code so that our system can flag answers for different wikipedia formats.  I left the log in my py file so that you all can see the types of questions it can currently answer.

--- QAProject/qa-system.py
+++ QAProject/qa-system.py
@@ -3,6 +3,9 @@ import sys
 import logging
 import spacy
 import re
+from nltk import sent_tokenize
+
+#notes - need to add additional date formats, need to make sure there is way to distinguish birthday's from dates,  need to add location regex
 
 # initialize spaCy
 nlp = spacy.load('en_core_web_sm')
@@ -18,7 +21,7 @@ patterns = {
     'When': [
         r"{} was born on", r"{} was born", r"{} was born in", r"{} was founded on", r"{} was established on",
         r"{} happened on",
-        r"born on {} on", r"born on {} in", r"born on {}"
+        r"born on {} on", r"born on {} in", r"born on {}", r"{} was"
     ],
     'Where': [
         r"{} is located in", r"{} is found in", r"{} is situated in", r"{} is in", r"{} is at"
@@ -35,13 +38,47 @@ def compile_patterns():
 
 compiled_patterns = compile_patterns()
 
-BIRTH_DATE_PATTERN = re.compile(r"\((\w+ \d{1,2}, \d{4}) –")
+BIRTH_DATE_PATTERN = re.compile(r"\((\w+ \d{1,2}, \d{4}) â")
 
-def check_birth_date_format(summary, subject):
-    match = BIRTH_DATE_PATTERN.search(summary)
+DATE_PATTERN = re.compile(r"\((\d{1,2} \w+ \d{4}) â")
+DATEPATTERN1 = re.compile(r'\d{1,2} \w+ \d{4}')
+datepattern2 = re.compile(r"\((\w+ \d{1,2}, \d{4}) â")
+
+locationpattern = re.compile(r'\blocated.*\b')
+locationpattern1 = re.compile(r'in [^.]*\.')
+
+def check_birth_date_format(summary, subject, question):
+    if 'born'in question:
+        match = BIRTH_DATE_PATTERN.search(summary)
+        if match:
+            birth_date = match.group(1)
+            return f"{subject} was born on {birth_date}."
+    return None
+
+def check_date_pattern(summary, subject):
+   match = DATE_PATTERN.search(summary)
+   match1 = DATEPATTERN1.search(summary)
+   match2 = datepattern2.search(summary)
+   if match1:
+       date = match1.group(0)
+       return f"{subject} started on {date}."
+   if match:
+       date = match.group(1)
+       return f"{subject} started on {date}."
+   if match2:
+       date = match2.group(1)
+       return f"{subject} started on {date}."
+   return None
+
+def check_location_pattern(summary, subject):
+    match = locationpattern.search(summary)
+    match1 = locationpattern1.search(summary)
     if match:
-        birth_date = match.group(1)
-        return f"{subject} was born on {birth_date}."
+        location = match.group(0)
+        return f"{subject} is {location}." 
+    if match1:
+        location = match1.group(0)
+        return f"{subject} is {location}"
     return None
 
 def setup_logging(logfile):
@@ -65,7 +102,7 @@ def get_wikipedia_summary(subject):
         logging.info(f"General error: {ex}")
         return None
 
-def find_answer(question_type, subject):
+def find_answer(question_type, subject, question):
     # generate search patterns
     search_patterns = compiled_patterns[question_type]
     logging.info(f"search patterns: {search_patterns}")
@@ -88,14 +125,24 @@ def find_answer(question_type, subject):
 
     # WHEN check birth date for when questions
     if question_type == 'When':
-        birth_date_answer = check_birth_date_format(summary, subject)
+        birth_date_answer = check_birth_date_format(summary, subject, question)
+        date_answer = check_date_pattern(summary, subject)
         if birth_date_answer:
             return birth_date_answer
+        if date_answer:
+            return date_answer
     # WHAT Check for a simple answer if the question type is 'What'
     if question_type == 'What':
-        return f"{summary}."
+        return f"{summary}"
     if question_type == 'Who':
-        return f"{summary}."
+        my_regex = "\(.*\)|\s-\s.*"
+        sent_tokens = sent_tokenize(summary)
+        summary1 = re.sub(my_regex, "", sent_tokens[0])
+        return f"{summary1}"
+    if question_type == 'Where' :
+        location_answer = check_location_pattern(summary, subject)
+        if location_answer:
+            return location_answer
 
     return None
 
@@ -130,15 +177,16 @@ def log_and_print(message, level='info'):
 
 def main():
     if len(sys.argv) != 2:
+        print(len(sys.argv))
         print("usage: python qa-system.py <logfile>")
         return
 
     logfile = sys.argv[1]
+  
     setup_logging(logfile)
     #logging.basicConfig(filename=logfile, level=logging.INFO)
 
-    print(
-        "*** This is a QA system by Group 5. It will try to answer questions that start with Who, What, When or Where. Enter 'exit' to leave the program.")
+    print("*** This is a QA system by Group 5. It will try to answer questions that start with Who, What, When or Where. Enter 'exit' to leave the program.")
 
     while True:
         question = input("=?> ")
@@ -160,7 +208,7 @@ def main():
             continue
 
         # find the answer
-        answer = find_answer(question_type, subject)
+        answer = find_answer(question_type, subject, question)
         if answer:
             log_and_print(f"Answer => {answer}", 'info')
             #print(f"=> {answer}")
